5a6ed208ac788220da7b64770bb104ed2b5e2ac3
Bhaskar joshi <mikechandrajoshi@gmail.com>
2021-11-30 22:01:38 +0530

    first commit

---
+++ Learning.py
@@ -0,0 +1,8 @@
+adj=input("adjective: ")
+verb1=input("verb1: ")
+verb2=input("verb2: ")
+famous_person=input("famous_person")
+
+matlib=f"Computer Programming is so {adj}! It makes me so excited all the time because \ I love to {verb1}.Stay hydrated and {verb2} like you ar a {famous_person}"
+print(matlib)
+
